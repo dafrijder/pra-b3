82e5b78825a2823023143bebc1e9ceb473a8cc84
Dafrijder <nielsdegast@outlook.com>
2025-02-28 10:04:32 +0100

    added the task controler

--- task/addtask.php
+++ task/addtask.php
@@ -7,9 +7,15 @@
 </head>
 
 <body>
-    
-    <div class="container">
 
+    <div class="container">
+        <div class="addtask-container">
+            <div class="addtask-info">
+                <h1>Add a task</h1>
+
+            </div>
+            <form action="" method="post"></form>
+        </div>
 
     </div>
 
