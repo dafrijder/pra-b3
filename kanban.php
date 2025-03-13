76da957dcc68426409dc4d4c46102600f24b6064
dgg0012 <dgg0012@edu.curio.nl>
2025-03-13 15:00:44 +0100

    updated kanban.php

--- kanban.php
+++ kanban.php
@@ -4,16 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kanban</title>
+    <link rel="stylesheet" href="css/kanban.css">
 </head>
-<body>
+<header>
     <?php require_once "header.php"?>
+</header>
+<body>
     <main>
+        <img src="" alt="logo">
         <div class="">
             <h1>Kanban</h1>
-        </div class="">
-            
         </div>
+        <div class='navbar'>
+
+        </div>
+        <div class="container">
+            <div class="box">
+                <p>To do</p>
+            </div>
+            <div class="box">
+                <p>In progress</p>
+            </div>
+            <div class="box">
+                <p>Done</p>
+            </div>
+            <div class="big-box">
+
+            </div>
+            <div class="big-box">
+
+            </div>
+            <div class="big-box">
+                
+            </div>
+        </div>
+
     </main>
-    <?php require_once "footer.php"?>
 </body>
+<footer>
+    <?php require_once "footer.php"?>
+</footer>
 </html>
