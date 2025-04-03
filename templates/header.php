969c72bffca17166cbeb5bb51d9355e2f8268ca7
Dafrijder <nielsdegast@outlook.com>
2025-04-03 11:15:48 +0200

    navbar

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,8 @@
 <header>
     <nav>
+        <div class="logo">
+            <a href="<?php $base_url?>/index.php"><img src="<?php $base_url?>/img/logo-big-v4.png" alt="logo"></a>
+        </div>
         <div class="navlinks">
             <a href="<?php $base_url?>/index.php">home</a>
             <a href="<?php $base_url?>/kanban.php">kanban</a>
