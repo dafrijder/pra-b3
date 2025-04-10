d988667db966f06def0153c37ebe9909d5b84e60
dgg0012 <dgg0012@edu.curio.nl>
2025-04-10 14:50:32 +0200

    Made a lot of changes to kanban.php

--- kanban.php
+++ kanban.php
@@ -1,3 +1,7 @@
+<?php 
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,16 +15,24 @@
 </header>
 <body>
     <main>
-        <div class="titlebox">
-            <div class="title">
-                <img src="" alt="logo">
-                <h1>Kanban</h1>
+        <div class="namethislater">
+            <div class="titlebox">
+                <div class="title">
+                    <img src="" alt="logo">
+                    <h1>Kanban</h1>
+                </div>
+            </div>
+            <div class="login-box">
+                <div class="login">
+                    <!--<p>Ingelogd als: <?php echo $_SESSION['username']; ?></p>!-->
+                    <a href="logout.php">Uitloggen</a>
+                </div>
             </div>
         </div>
         <div class='navbar'>
 
         </div>
-        <div class="container">
+        <div class="box-container">
             <div class="box">
                 <p>To do</p>
             </div>
@@ -30,14 +42,68 @@
             <div class="box">
                 <p>Done</p>
             </div>
-            <div class="big-box">
-
+        </div>
+        <?php 
+        require_once "backend/conn.php";
+        $query = "SELECT title,description,department FROM kanban";
+        $statement=$conn->prepare($query);
+        $statement->execute();
+        $kanban = $statement->fetchall(PDO::FETCH_ASSOC);
+        ?>
+        <div class="bigbox-container">
+            <div class="to-do">
+                <div class="task-box">       
+                    <?php 
+                        foreach($kanban as $task){
+                            if($task['department']=="to-do"){
+                                echo "<div class='task'>";
+                                echo "<h2>".$task['title']."</h2>";
+                                echo "<p>".$task['description']."</p>";
+                                echo "<a href=''>Update</a>";
+                                echo "<a href=''>Delete</a>";
+                                echo "</div>";
+                            }
+                            else{
+                                continue;
+                            }
+                    }?>
+                </div>
             </div>
-            <div class="big-box">
-
+            <div class="in-progress">
+                <div class="task-box">
+                    <?php 
+                        foreach($kanban as $task){
+                            if($task['department']=="in-progress"){
+                                echo "<div class='task'>";
+                                echo "<h2>".$task['title']."</h2>";
+                                echo "<p>".$task['description']."</p>";
+                                echo "<a href=''>Update</a>";
+                                echo "<a href=''>Delete</a>";
+                                echo "</div>";
+                            }
+                            else{
+                                continue;
+                            }
+                    }?>
+                </div>
             </div>
-            <div class="big-box">
-                
+            <div class="done">
+                <div class="task-box">
+                    <?php 
+                    foreach($kanban as $task){
+                        if($task['department']=="done"){
+                            echo "<div class='task'>";
+                            echo "<h2>".$task['title']."</h2>";
+                            echo "<p>".$task['description']."</p>";
+                            echo "<a href=''>Update</a>";
+                            echo "<a href=''>Delete</a>";
+                            echo "</div>";
+                        }
+                        else{
+                            continue;
+                        }
+                    }?>
+                </div>
             </div>
         </div>
 
